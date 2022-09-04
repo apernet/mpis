5664919bb7cf5eca687bae5b7e5ddcce1ba2982f
Honghao Zeng <nat@nat.moe>
2022-09-03 23:07:29 -0700

    test: frag & bilateral comm

--- test.sh
+++ test.sh
@@ -63,26 +63,32 @@ ip -netns "${node1}" -4 addr add 1.2.3.4/32 dev lo
 ip -netns "${node1}" -4 addr add 11.22.33.44/32 dev lo
 ip -netns "${node1}" link set lo up
 
+# route lo traffic to node1
+ip -netns "${send}" -4 route add 1.2.3.4/32 via 10.1.0.1
+ip -netns "${send}" -4 route add 11.22.33.44/32 via 10.1.0.1
+
 # node2 tunnel ip on loopback
 ip -netns "${node2}" -4 addr add 5.6.7.8/32 dev lo
 ip -netns "${node2}" -4 addr add 55.66.77.88/32 dev lo
 ip -netns "${node2}" link set lo up
 
 cat > test-send.cfg << EOF
-from 1.2.3.0/24 encap 10.0.0.254 cutoff-ttl 10
+iif link-1s src 1.2.3.0/24 encap 10.0.0.254 cutoff-ttl 10
+iif link-sr dst 10.0.0.1 decap 5.6.7.0/24
 EOF
 
 cat > test-recv.cfg << EOF
-to 10.0.0.254 decap 1.2.3.0/24
+iif link-rs dst 10.0.0.254 decap 1.2.3.0/24
+iif link-r2 src 5.6.7.0/24 encap 10.0.0.1 cutoff-ttl 10
 EOF
 
 # mpis on sender
-ip netns exec "${send}" ./mpis-routectl -t test-send.cfg -e mpis-ebpf.o link-1s
+ip netns exec "${send}" ./mpis-routectl -t test-send.cfg -e mpis-ebpf.o link-1s link-sr
 
 # mpis on receiver
-ip netns exec "${recv}" ./mpis-routectl -t test-recv.cfg -e mpis-ebpf.o link-rs
+ip netns exec "${recv}" ./mpis-routectl -t test-recv.cfg -e mpis-ebpf.o link-rs link-r2
 
-ip netns exec "${node1}" ping -I 1.2.3.4 5.6.7.8 -c10 > /dev/null 2>&1 &
+ip netns exec "${node1}" ping -I 1.2.3.4 5.6.7.8 -c20 > /dev/null 2>&1 &
 ping_pid_1=$!
 
 echo '==== tests started ===='
@@ -92,6 +98,12 @@ timeout 5s ip netns exec "${send}" tcpdump -i link-sr -n 'icmp and src 5.6.7.8 a
 
 echo -n 'testing decap... '
 timeout 5s ip netns exec "${recv}" tcpdump -i link-rs -n 'icmp and src 1.2.3.4 and dst 5.6.7.8' -c1  > /dev/null 2>&1 && echo 'ok' || echo 'failed'
+
+echo -n 'testing encap (inverse)... '
+timeout 5s ip netns exec "${recv}" tcpdump -i link-rs -n 'icmp and src 1.2.3.4 and dst 10.0.0.1' -c1 > /dev/null 2>&1 && echo 'ok' || echo 'failed'
+
+echo -n 'testing decap (inverse)... '
+timeout 5s ip netns exec "${send}" tcpdump -i link-sr -n 'icmp and dst 1.2.3.4 and src 5.6.7.8' -c1  > /dev/null 2>&1 && echo 'ok' || echo 'failed'
 
 ip netns exec "${node1}" ping -I 11.22.33.44 55.66.77.88 -c10 > /dev/null 2>&1 &
 ping_pid_2=$!
@@ -107,6 +119,9 @@ ping_pid_3=$!
 
 echo -n 'testing ttl-cutoff... '
 timeout 5s ip netns exec "${send}" tcpdump -i link-sr -n 'icmp and src 1.2.3.4 and dst 10.0.0.254' -c1 > /dev/null 2>&1 && echo 'ok' || echo 'failed'
+
+echo -n 'testing fragmentation... '
+timeout 10 ip netns exec "${node1}" ping -I 1.2.3.4 5.6.7.8 -c1 > /dev/null 2>&1 && echo 'ok' || echo 'failed'
 
 kill $ping_pid_1
 kill $ping_pid_2
